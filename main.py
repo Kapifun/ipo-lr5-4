48e8e083c8e9801204bb2d7aa829d5cf95b25756
Kapifun <kirill.doronin.07@mail.ru>
2024-12-14 10:16:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 string=input("Введите строку:")#ввод строки
-
 words=string.split()#инициализация переменной words
 string1=''#инициализация переменной string
 for word in words:#цикл for
         string1 += word[0]#инициализация первых букв
-
 print("Результат:",string1)#вывод результата
